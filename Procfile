12d570992eb69f7b0f97ed4b286febd6ef1a3b75
rabbilyasar <rabbilyasar@gmail.com>
2021-05-26 13:07:25 +0600

    heroku
    Signed-off-by: rabbilyasar <rabbilyasar@gmail.com>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn auction_site/app.wsgi --log-file -
--- auction_site/app/settings.py
+++ auction_site/app/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = '2zpxjj8#uo4rimk^)0k8m##8g&#01ta3y!2zk%%kj=5k@8qvo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'rabbilable-auction-site.herokuapp.com/']
 
 
 # Application definition
@@ -126,9 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = '/static/'
+# STATIC_ROOT = '/static/'
 # STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
